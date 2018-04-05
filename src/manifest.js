d32c83b3b3f07af929aec6658f09cdd5b8f96f82
Shiv Prasad <shiv@param.ai>
2018-04-05 18:16:58 +0530

    Updated app title

--- src/manifest.js
+++ src/manifest.js
@@ -21,7 +21,7 @@ module.exports = {
     'identity.email'
   ],
   browser_action: {
-    default_title: 'title',
+    default_title: 'FimlyQuote',
     default_popup: 'pages/popup.html'
   },
   background: {
